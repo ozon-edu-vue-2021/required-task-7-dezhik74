f45b03aeba1abfe7df1bf668d5b893f156371aad
dezhik74 <dezhik@yandex.ru>
2021-11-30 23:49:26 +0300

    0.2 я сделал не друзей, а фоловеров!

--- src/index.js
+++ src/index.js
@@ -2,56 +2,83 @@
 
 import css from "./css/style.css"
 
-import originalData from "./data.json"
+import {matrixData, best} from "./store/store"
 
-console.log("original ", originalData)
+// console.log("matrix ", matrixData)
+// console.log("best ", best)
 
-//уменьшаем все индексы на 1, чтобы ардесоваться как в массиве 
-// плюс сортируем friends
-let matrixData = originalData.map(itm => {
-  let newFriends = itm.friends.map((fr) => fr - 1).sort((a,b) => a-b)
-  return {
-    id: itm.id - 1,
-    name: itm.name,
-    friends: newFriends,
-    followers: [],
-    enemyes: [], 
+const listView = document.querySelector(".list-view")
+const contactList = document.querySelector(".contacts-list")
+const userComponent = document.querySelector(".details-view")
+
+let backButton = null
+
+function showUser (user, best) {
+  // вот здесь режутся друзья и враги до 3 элементов
+  const followers = user.followers.slice(0, 3).map(itm => {
+    return `<li><i class="fa fa-male"></i><span >${matrixData[itm].name}</span></li>`
+  }).join(' ')
+  const enemyes = user.enemyes.slice(0, 3).map(itm => {
+    return `<li><i class="fa fa-male"></i><span >${matrixData[itm].name}</span></li>`
+  }).join(' ')
+  const theBests = best.map(itm => {
+    return `<li><i class="fa fa-male"></i><span >${itm.name}</span></li>`
+  }).join(' ')
+  const userHTML = `
+    <div class="user-animation">
+      <div class="background">
+        <div class="back"></div>
+        <div class="user-img"></div>
+        <div class="user-name">${user.name}</div>
+      </div>
+      <ul>
+        <li class="people-title">Фоловеры</li>
+        ${followers}
+        <li class="people-title">Не в друзьях</li>
+        ${enemyes}
+        <li class="people-title">Популярные люди</li>
+        ${theBests}
+      </ul>
+    </div>
+  `
+  window.scrollTo(0, 0)
+  listView.classList.add("hidden")
+  userComponent.classList.remove("hidden")
+  const elem = document.createElement('div')
+  elem.innerHTML = userHTML
+  userComponent.append(elem)
+  //стилизуем дополнительно
+  const arrayOfLi = userComponent.getElementsByTagName("li")
+  for (let idx = 0; idx < arrayOfLi.length; idx++) {
+    arrayOfLi[idx].style.transform = `translate3d(0, ${30 + idx * 5}px, 0)`
+    arrayOfLi[idx].style.WebkitTransform = `translate3d(0, ${30 + idx * 5}px, 0)`
   }
-})
+  //настраиваем кнопку
+  backButton = userComponent.querySelector(".back")
+  backButton.addEventListener("click", showClassList)
+}
 
+function createContactListItem (user, idx) {
+  let elem = document.createElement('li')
+  elem.className = "contact-list__item"
+  elem.innerHTML=`<strong class="item-userame">${user.name}</strong>`
+  elem.onclick = () => {showUser(user, best)}
+  contactList.append(elem)
+}
+    
+function createContactList (){
+  matrixData.forEach((item, idx) => {createContactListItem(item, idx)})
+}
 
-// для каждого юзера раскидываем его id по тем, у кого он в друзьях. Получется заполнен массив followers
-matrixData.forEach(item => {
-  item.friends.forEach(friend => {
-    matrixData[friend].followers.push(item.id)
-  })
-})
+function showClassList() {
+  userComponent.innerHTML=""
+  listView.classList.remove("hidden")
+  backButton.removeEventListener("click", showClassList)
+  
+}
 
-// проходим по всем юзерам и массив enemyes записываем числа от 0 до matrixData.length, не совпадающие с friends
-const matrixSize = matrixData.length
-matrixData.forEach(item => {
-  for (let i=0; i<matrixSize; i++) {
-    if (!(i in item.followers)) {
-      item.enemyes.push(i)
-    }
-  }
-})
-console.log("matrix before sort", matrixData)
+function init () {
+  createContactList()
+}
 
-//в свойстве user.followers.length лежит число фоловеров каждого пользователя.
-//Надо как-то отбрать троих самых популярных.
-//причем если есть конкуренты то по алфавиту
-//первый путь - две сортировки - 1) по алфавиту на возрастание 2) по фоловерам на убывание
-
-matrixData.sort((a,b) => {
-  if (a.name > b.name) return 1;
-  if (a.name < b.name) return -1;
-  if (a.name == b.name) return 0;
-
-})
-
-matrixData.sort((a,b) => {return b.followers.length - a.followers.length})
-
-let best = matrixData.slice(0, 3)
-
-console.log("best ", best)
+init ()
---
+++ src/store/store.js
@@ -0,0 +1,53 @@
+import originalData from "../data.json"
+
+// console.log("original ", originalData)
+
+//уменьшаем все индексы на 1, чтобы ардесоваться как в массиве 
+// плюс сортируем friends
+export let matrixData = originalData.map(itm => {
+  let newFriends = itm.friends.map((fr) => fr - 1).sort((a,b) => a-b)
+  return {
+    id: itm.id - 1,
+    name: itm.name,
+    friends: newFriends,
+    followers: [],
+    enemyes: [], 
+  }
+})
+
+
+// для каждого юзера раскидываем его id по тем, у кого он в друзьях. Получется заполнен массив followers
+matrixData.forEach(item => {
+  item.friends.forEach(friend => {
+    matrixData[friend].followers.push(item.id)
+  })
+})
+
+// проходим по всем юзерам и массив enemyes записываем числа от 0 до matrixData.length, не совпадающие с friends
+const matrixSize = matrixData.length
+matrixData.forEach(item => {
+  for (let i=0; i<matrixSize; i++) {
+    if (!(i in item.followers)) {
+      item.enemyes.push(i)
+    }
+  }
+})
+// console.log("matrix before sort", matrixData)
+
+//в свойстве user.followers.length лежит число фоловеров каждого пользователя.
+//Надо как-то отбрать троих самых популярных.
+//причем если есть конкуренты то по алфавиту
+//первый путь - две сортировки - 1) по алфавиту на возрастание 2) по фоловерам на убывание
+
+matrixData.sort((a,b) => {
+  if (a.name > b.name) return 1;
+  if (a.name < b.name) return -1;
+  if (a.name == b.name) return 0;
+
+})
+
+matrixData.sort((a,b) => {return b.followers.length - a.followers.length})
+
+// итого - 2*O(N) потом O(N**2), потом две сортировки это примерно 2*O(logN)
+export let best = matrixData.slice(0, 3)
+
